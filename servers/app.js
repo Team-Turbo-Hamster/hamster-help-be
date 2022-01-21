94cd8984809369a499f8e9ad150a42162db567f7
Matthew C Wake <mcwake-dev@outlook.com>
2022-01-21 11:00:10 +0000

    Moved servers to folders. Added stub for passport authentication.

--- servers/app.js
+++ servers/app.js
@@ -4,15 +4,19 @@ const app = express();
 const { v4: uuidv4 } = require("uuid");
 const session = require("express-session");
 const morgan = require("morgan");
-const apiRouter = require("./routes/apiRouter");
-const { handleCustomErrors, handleServerErrors } = require("./errors");
-
+const apiRouter = require("../routes/apiRouter");
+const { handleCustomErrors, handleServerErrors } = require("../errors");
+const sessionMiddleware = session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialized: false,
+});
 app.use(morgan("combined"));
 app.use(cors());
-app.use(session({ secret: uuidv4(), resave: false, saveUninitialized: false }));
+app.use(sessionMiddleware);
 app.use("/api", apiRouter);
 app.use("", (req, res, next) => next({ status: 404, msg: "Not Found" }));
 app.use(handleCustomErrors);
 app.use(handleServerErrors);
 
-module.exports = app;
+module.exports = { app, sessionMiddleware };
--- servers/socket.js
+++ servers/socket.js
@@ -1,11 +1,24 @@
 const socketio = require("socket.io");
 
-module.exports = (httpServer) => {
+module.exports = (httpServer, sessionMiddleware, passport) => {
+  const wrap = (middleware) => (socket, next) =>
+    middleware(socket.request, {}, next);
   const io = new socketio.Server(httpServer, {
     cors: {
       origin: process.env.CLIENT_URL,
     },
   });
+
+  io.use(wrap(sessionMiddleware));
+  /*  io.use(wrap(passport.initialize()));
+  io.use(wrap(passport.session()));
+  io.use((socket, next) => {
+    if (socket.request.user) {
+      next();
+    } else {
+      next(new Error("unauthorized"));
+    }
+  });*/
 
   io.on("connection", (socket) => {
     // If you want to immediately trigger something on first connection, add it here
