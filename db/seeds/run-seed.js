6622626afceec2b0910277da24baabbc812da2e1
Matthew C Wake <mcwake-dev@outlook.com>
2022-01-23 20:35:53 +0000

    Added seed data for tickets

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -5,25 +5,61 @@ require("dotenv").config({
 
 const mongoose = require("mongoose");
 const User = require("../../models/user.model");
+const Ticket = require("../../models/ticket.model");
 const userData = require(`../data/${process.env.NODE_ENV}-data/users.js`);
 
-mongoose
-  .connect(process.env.DATABASE, {
+const runSeed = async () => {
+  const connection = await mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
+  });
+
+  console.log("Removing existing data...");
+  await User.deleteMany();
+  console.log("---> Completed");
+
+  console.log("Adding new users...");
+  await User.insertMany(
+    userData.map(({ name, email, avatar, role }) => ({
+      name,
+      email,
+      avatar,
+      role,
+    }))
+  );
+  console.log("---> Completed");
+
+  console.log("Retrieving users from database...");
+  const users = await User.find();
+  console.log("---> Completed");
+
+  console.log("Adding user tickets...");
+  users.forEach(async (user) => {
+    const userTickets = userData.find(
+      (seedUser) => seedUser.email === user.email
+    ).tickets;
+
+    if (userTickets.length > 0) {
+      await Ticket.insertMany(
+        userTickets.map(({ title, body, tags, image }) => ({
+          user: mongoose.Types.ObjectId(user._id),
+          title,
+          body,
+          tags,
+          image,
+        }))
+      );
+    }
+  });
+  console.log("---> Completed");
+
+  return true;
+};
+
+runSeed()
   .then(() => {
-    console.log("Seeding users...");
-    User.deleteMany().then(() => {
-      User.insertMany(userData)
-        .then((result) => {
-          console.log("--> User seeding complete");
-        })
-        .catch((err) => {
-          console.log("!!! Error seeding users: ", err);
-        });
-    });
+    console.log("*** SEEDING COMPLETE ***");
   })
   .catch((err) => {
-    console.log("Mongoose Connection Failed!", err);
+    console.log("Seeding Failed!", err);
   });
