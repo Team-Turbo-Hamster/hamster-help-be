96420fab87d679f66cdab10e71950aa473b4db9c
Matthew C Wake <mcwake-dev@outlook.com>
2022-01-25 15:48:50 +0000

    Fixed ticket seeding issues (Hopefully...)

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,3 @@
-const env = require("../../environment/test");
 const mongoose = require("mongoose");
 const User = require("../../models/user.model");
 const Ticket = require("../../models/ticket.model");
@@ -10,14 +9,8 @@ const runSeed = async () => {
     useUnifiedTopology: true,
   });
 
-  console.log("Removing existing data...");
-  console.log("> Removing Users...");
   await User.deleteMany();
-  console.log("> Removing Tickets...");
   await Ticket.deleteMany();
-  console.log("---> Completed");
-
-  console.log("Adding new users...");
   await User.insertMany(
     userData.map(({ name, email, avatar, role }) => ({
       name,
@@ -26,13 +19,9 @@ const runSeed = async () => {
       role,
     }))
   );
-  console.log("---> Completed");
 
-  console.log("Retrieving users from database...");
   const users = await User.find();
-  console.log("---> Completed");
 
-  console.log("Adding user tickets...");
   users.forEach(async (user) => {
     const userTickets = userData.find(
       (seedUser) => seedUser.email === user.email
@@ -50,7 +39,6 @@ const runSeed = async () => {
       );
     }
   });
-  console.log("---> Completed");
 };
 
 module.exports = runSeed;
