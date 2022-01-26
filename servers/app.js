b6dc7da511ae783c46ce6e4e41e6026661c28e34
Matthew C Wake <mcwake-dev@outlook.com>
2022-01-26 09:13:58 +0000

    Removed leftover session middleware

--- servers/app.js
+++ servers/app.js
@@ -7,11 +7,7 @@ const {
   handleServerErrors,
   handleMongooseErrors,
 } = require("../errors");
-const sessionMiddleware = session({
-  secret: uuidv4(),
-  resave: false,
-  saveUninitialized: false,
-});
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
